2294cd31e27afe022f813cda07df3228c113f84d
amit-purohit <purohitamit2001@gmail.com>
2021-10-06 22:39:52 +0530

    menus and topmenus created

--- app/src/main/java/com/amit/chatsapp/MainActivity.java
+++ app/src/main/java/com/amit/chatsapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.amit.chatsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId())
+        {
+            case R.id.search:
+                Toast.makeText(this, "Search Clicked", Toast.LENGTH_LONG).show();
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.topmenu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
