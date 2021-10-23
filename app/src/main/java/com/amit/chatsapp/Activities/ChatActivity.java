fa28f5397b5d346b2f302dc3160bd411da4291dd
amit-purohit <purohitamit2001@gmail.com>
2021-10-23 18:03:40 +0530

    6>. sending and receiving message

--- app/src/main/java/com/amit/chatsapp/Activities/ChatActivity.java
+++ app/src/main/java/com/amit/chatsapp/Activities/ChatActivity.java
@@ -1,6 +1,8 @@
 package com.amit.chatsapp.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +12,10 @@ import com.amit.chatsapp.Models.Message;
 import com.amit.chatsapp.databinding.ActivityChatBinding;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -32,6 +37,8 @@ public class ChatActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         messages = new ArrayList<>();
         adapter = new MessagesAdapter(this, messages);
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        binding.recyclerView.setAdapter(adapter);
 
 
 
@@ -46,21 +53,45 @@ public class ChatActivity extends AppCompatActivity {
         receiverRoom = receiverUid + senderUid;
         database = FirebaseDatabase.getInstance();
 
+        database.getReference().child("chats")
+                .child(senderRoom)
+                .child("messages")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull  DataSnapshot snapshot) {
+                        messages.clear();
+                        for(DataSnapshot snapshot1:snapshot.getChildren())
+                        {
+                            Message message = snapshot1.getValue(Message.class);
+                            messages.add(message);
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull  DatabaseError error) {
+
+                    }
+                });
+
         binding.senBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String messageTxt = binding.messageBox.getText().toString();
                 Date date = new Date();
                 Message message = new Message(messageTxt, senderUid, date.getTime());
+                binding.messageBox.setText("");
                 database.getReference().child("chats")
                         .child(senderRoom)
                         .child("messages")
+                        .push()
                         .setValue(message).addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void unused) {
                         database.getReference().child("chats")
                                 .child(receiverRoom)
                                 .child("messages")
+                                .push()
                                 .setValue(message).addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
                             public void onSuccess(Void unused) {
